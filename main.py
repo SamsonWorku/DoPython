08111a769fcc86136b5123e8911b90326c0b7c01
SamsonWorku <48730941+SamsonWorku@users.noreply.github.com>
2020-06-14 00:27:49 -0400

    initial code

---
+++ main.py
@@ -0,0 +1,25 @@
+#VARIABLE
+print("Sami")
+x=15
+y=45
+z=x+y
+print(z)
+#-------------------------------------------
+X,Y,Z=23,22,11
+Z=X+Y+Z
+print(Z)
+
+fname="samson"
+lname="worku"
+age = 45
+#age_= "age is"
+#fname= fname+ lname
+print(fname+ " " + lname+ " "+ "age_" + str(age))
+print(type(z))
+x= """A subquery is a query inside another query where a query is defined to retrieve data or information back from the database. In a subquery, the outer query is called as the main query whereas the inner query is called subquery. Subqueries are always executed first and the result of the subquery is passed on to the main query. It can be nested inside a SELECT, UPDATE or any other query. A subquery can also use any comparison operators such as >,< or =.
+The properties of subqueries are as follows: A subquery is a query inside another query where a query is defined to retrieve data or information back from the database. In a subquery, the outer query is called as the main query whereas the inner query is called subquery. Subqueries are always executed first and the result of the subquery is passed on to the main query. It can be nested inside a SELECT, UPDATE or any other query. A subquery can also use any comparison operators such as >,< or =.
+The properties of subqueries are as follows: A subquery is a query inside another query where a query is defined to retrieve data or information back from the database. In a subquery, the outer query is called as the main query whereas the inner query is called subquery. Subqueries are always executed first and the result of the subquery is passed on to the main query. It can be nested inside a SELECT, UPDATE or any other query. A subquery can also use any comparison operators such as >,< or =.
+The properties of subqueries are as follows: """
+print(len(x))
+print(dir(x))
+print(help(str))
